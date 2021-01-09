fd7820fdfcd02f5fc70454afe83588a3a1718f16
jr31112 <jr435@naver.com>
2021-01-09 23:58:13 +0900

    210109 | Bean Find (find Bean by type)

--- src/test/java/com/project/delivery/beanTest/ApplicationContextExtendsFindTest.java
+++ src/test/java/com/project/delivery/beanTest/ApplicationContextExtendsFindTest.java
@@ -33,6 +33,32 @@ public class ApplicationContextExtendsFindTest {
         assertThat(rateDiscountPolicy).isInstanceOf(RateDiscountPolicy.class);
     }
 
+    @Test
+    @DisplayName("특정 하위 타입으로 조회")
+    void findBeanBySubType() {
+        RateDiscountPolicy bean = ac.getBean(RateDiscountPolicy.class);
+        assertThat(bean).isInstanceOf(RateDiscountPolicy.class);
+    }
+
+    @Test
+    @DisplayName("부모 타입으로 모두 조회하기")
+    void findAllBeanByParentType() {
+        Map<String, DiscountPolicy> beansOfType = ac.getBeansOfType(DiscountPolicy.class);
+        assertThat(beansOfType.size()).isEqualTo(2);
+        for (String key : beansOfType.keySet()) {
+            System.out.println("key = " + key + " value=" + beansOfType.get(key));
+        }
+    }
+
+    @Test
+    @DisplayName("부모 타입으로 모두 조회하기 - Object")
+    void findAllBeanByObjectType() {
+        Map<String, Object> beansOfType = ac.getBeansOfType(Object.class);
+        for (String key : beansOfType.keySet()) {
+            System.out.println("key = " + key + " value=" + beansOfType.get(key));
+        }
+    }
+
     @Configuration
     static class TestConfig {
         @Bean
