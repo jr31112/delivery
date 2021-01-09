830cebc48d3c22d1892d93a607cfa06bfc33a145
jr31112 <jr435@naver.com>
2021-01-09 16:13:47 +0900

    210109 | Bean Find (find Bean by Type)

--- src/test/java/com/project/delivery/beanTest/ApplicationContextBeanFindTest.java
+++ src/test/java/com/project/delivery/beanTest/ApplicationContextBeanFindTest.java
@@ -3,7 +3,10 @@ package com.project.delivery.beanTest;
 import com.project.delivery.AppConfig;
 import com.project.delivery.member.MemberService;
 import com.project.delivery.member.MemberServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
@@ -13,14 +16,23 @@ public class ApplicationContextBeanFindTest {
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
     @Test
+    @DisplayName("빈 이름으로 조회")
     public void findBeanByName(){
         MemberService memberService = ac.getBean("memberService", MemberService.class);
         assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
     }
 
     @Test
+    @DisplayName("빈 타입으로조회")
     public void findBeanByType(){
         MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanByName2() {
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
         assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
     }
 
